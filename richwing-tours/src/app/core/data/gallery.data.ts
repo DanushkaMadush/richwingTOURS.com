b2d691a86e8bbd026d9b5905f9ba681e3522dd8d
danushka <danmadushzxc@gmail.com>
2026-06-08 21:03:51 +0530

    migrated gallery page

--- richwing-tours/src/app/core/data/gallery.data.ts
+++ richwing-tours/src/app/core/data/gallery.data.ts
@@ -3,5 +3,22 @@ import { GalleryImage } from '../models/gallery.model';
 export const GALLERY_IMAGES: GalleryImage[] = [
   { id: 'gallery-1', image: { src: '/images/Gallery/1.jpg', alt: 'Richwing Tours gallery image 1' } },
   { id: 'gallery-2', image: { src: '/images/Gallery/2.jpg', alt: 'Richwing Tours gallery image 2' } },
-  { id: 'gallery-3', image: { src: '/images/Gallery/3.jpg', alt: 'Richwing Tours gallery image 3' } }
+  { id: 'gallery-3', image: { src: '/images/Gallery/3.jpg', alt: 'Richwing Tours gallery image 3' } },
+  { id: 'gallery-4', image: { src: '/images/Gallery/4.jpg', alt: 'Richwing Tours gallery image 4' } },
+  { id: 'gallery-5', image: { src: '/images/Gallery/5.jpg', alt: 'Richwing Tours gallery image 5' } },
+  { id: 'gallery-6', image: { src: '/images/Gallery/6.jpg', alt: 'Richwing Tours gallery image 6' } },
+  { id: 'gallery-7', image: { src: '/images/Gallery/7.jpg', alt: 'Richwing Tours gallery image 7' } },
+  { id: 'gallery-8', image: { src: '/images/Gallery/8.jpg', alt: 'Richwing Tours gallery image 8' } },
+  { id: 'gallery-9', image: { src: '/images/Gallery/9.jpg', alt: 'Richwing Tours gallery image 9' } },
+  { id: 'gallery-10', image: { src: '/images/Gallery/10.jpg', alt: 'Richwing Tours gallery image 10' } },
+  { id: 'gallery-11', image: { src: '/images/Gallery/11.jpg', alt: 'Richwing Tours gallery image 11' } },
+  { id: 'gallery-12', image: { src: '/images/Gallery/12.jpg', alt: 'Richwing Tours gallery image 12' } },
+  { id: 'gallery-13', image: { src: '/images/Gallery/13.jpg', alt: 'Richwing Tours gallery image 13' } },
+  { id: 'gallery-14', image: { src: '/images/Gallery/14.jpg', alt: 'Richwing Tours gallery image 14' } },
+  { id: 'gallery-15', image: { src: '/images/Gallery/15.jpg', alt: 'Richwing Tours gallery image 15' } },
+  { id: 'gallery-16', image: { src: '/images/Gallery/16.jpg', alt: 'Richwing Tours gallery image 16' } },
+  { id: 'gallery-17', image: { src: '/images/Gallery/17.jpeg', alt: 'Richwing Tours gallery image 17' } },
+  { id: 'gallery-18', image: { src: '/images/Gallery/18.jpeg', alt: 'Richwing Tours gallery image 18' } },
+  { id: 'gallery-19', image: { src: '/images/Gallery/19.jpg', alt: 'Richwing Tours gallery image 19' } },
+  { id: 'gallery-20', image: { src: '/images/Gallery/20.jpg', alt: 'Richwing Tours gallery image 20' } }
 ];
